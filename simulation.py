4ab17053119492be0eda3b6fc1a86fe3ca18613a
Dean Hoffmann <dean.hoffmann@stud.uni-hannover.de>
2025-02-25 22:29:03 +0100

    rough finish

--- simulation.py
+++ simulation.py
@@ -2,6 +2,11 @@ import pygame
 import numpy as np
 import time
 import math
+import matplotlib.pyplot as plt
+
+import scipy.ndimage
+
+np.set_printoptions(threshold=np.inf)
 
 # Constants
 WIDTH, HEIGHT = 1600, 600
@@ -10,16 +15,16 @@ DRONE_RADIUS = 1
 DRONE_COLOR = (0, 0, 0)
 PATH_COLOR = (255, 255, 0)
 FPS = 30
-PHOTO_COLOR = (255, 0, 0)
+PHOTO_COLOR = (255, 0, 0, 25)
 PHOTO_SIZE = 19
-SIMULATION_SPEED = 5
+SIMULATION_SPEED = 50
 CENTER_X = WIDTH // 2
 CENTER_Y = HEIGHT // 2
 
-VERTICE_1 = (-100, -10)
-VERTICE_2 = (-10, -10)
-VERTICE_3 = (-100, -80)
-VERTICE_4 = (-10, -80)
+VERTICE_1 = (-100, -100)
+VERTICE_2 = (-100, 100)
+VERTICE_3 = (100, -100)
+VERTICE_4 = (100, 100)
 
 # Drone Simulation Object
 class DroneSimulator:
@@ -38,6 +43,9 @@ class DroneSimulator:
         self.flight_time = 0
         self.boundry_shape = boundry_shape
         self.boundry_params = boundry_params if boundry_params else {}
+
+        self.coverage_map = np.empty([WIDTH, HEIGHT])
+        self.coverage_map[:] = np.nan
 
     def adjust_flight_parameters(self, xVelocity, yVelocity, yaw):
         self.velocity = np.array([xVelocity, yVelocity], dtype=float)
@@ -74,8 +82,11 @@ class DroneSimulator:
         self.position += self.velocity * delta_time
         self.path.append(tuple(self.position.copy()))  # Store position history
         
-    def to_screen_coords(sef, pos):
+    def to_screen_coords(self, pos):
         return (int(pos[0] + CENTER_X), int(-pos[1] + CENTER_Y))
+    
+    def to_world_coords(self, pos):
+        return (int(pos[0] + CENTER_X), int(pos[1] + CENTER_Y))
 
     # currently not in use
     def handle_input(self):
@@ -106,15 +117,16 @@ class DroneSimulator:
             max_x = max(v[0] for v in screen_rect_vertices)
             min_y = min(v[1] for v in screen_rect_vertices)
             pygame.draw.rect(self.screen, (255, 0, 0), (min_x, min_y, (max_x - min_x), (max_y - min_y)), 1)
+            self.add_rectangle_boundry_to_coverage_map(min_x, max_x, min_y, max_y)
 
     def draw(self):
         self.screen.fill(BACKGROUND_COLOR)
 
         for point in self.path:
-            pygame.draw.circle(self.screen, PATH_COLOR, self.to_screen_coords(point), 2)
+            pygame.draw.circle(self.screen, PATH_COLOR, self.to_screen_coords(point), 1)
         for photo in self.photos:
             surf = pygame.Surface((PHOTO_SIZE, PHOTO_SIZE), pygame.SRCALPHA)
-            pygame.draw.rect(surf, PHOTO_COLOR, (0, 0, PHOTO_SIZE, PHOTO_SIZE), 2)
+            pygame.draw.rect(surf, PHOTO_COLOR, (0, 0, PHOTO_SIZE, PHOTO_SIZE), 0)
             rot_surf = pygame.transform.rotate(surf, photo[1] * -1)
             rot_rect = rot_surf.get_rect(center=self.to_screen_coords(photo[0]))
             self.screen.blit(rot_surf, rot_rect.topleft)
@@ -139,6 +151,76 @@ class DroneSimulator:
         seconds = int(self.flight_time % 60)
         print(f"Total flight time: {minutes} Minutes, {seconds} Seconds")
 
+    def add_rectangle_boundry_to_coverage_map(self, min_x, max_x, min_y, max_y):
+
+        for x in range(min_x, max_x + 1):
+            for y in range( min_y, max_y + 1):
+                self.coverage_map[int(x), int(y)] = 0
+
+    def add_circle_boundry_to_coverage_map(self, x, y, radius):
+        point = (x, y)
+        point = self.to_world_coords(point)
+        x, y = point
+        
+        for i in range(int(x - radius), int(x + radius) + 1):
+            for j in range(int(y - radius), int(y + radius) + 1):
+                # Check if the point is inside the circle
+                if (i - x) ** 2 + (j - y) ** 2 <= radius ** 2:
+                    self.coverage_map[int(i), int(j)] = 0
+
+    def add_photos_to_coverage_map(self):
+        for photo in self.photos:
+            center = photo[0]
+            half_size = PHOTO_SIZE // 2
+            photo_array = np.empty((PHOTO_SIZE, PHOTO_SIZE), dtype=object)
+
+            # Generate photos coordinate list
+            for i in range(PHOTO_SIZE):
+                for j in range(PHOTO_SIZE):
+                    x = center[0] - half_size + i
+                    y = center[1] - half_size + j
+                    photo_array[i, j] = (x, y)
+            
+            # Rotate each coordinate in the photo with given angle
+            angle = photo[1]
+            rotated_photo = [rotate_point(x, y, center[0], center[1], angle) for x, y in photo_array.flat]
+
+            # Update the coverage map
+            for p in rotated_photo:
+                p = self.to_world_coords(p)
+                x, y = p
+                if self.coverage_map[int(x), int(y)] >= 0:
+                    self.coverage_map[int(x), int(y)] += 1
+
+    def calculate_coverage(self):
+        area = 0
+        coverCount = 0
+
+        for (i, j), value in np.ndenumerate(self.coverage_map):
+            if value >= 0:
+                area += 1
+            if value > 0:
+                coverCount += 1
+        
+        coverage = int((coverCount / area) * 100)
+        print(f"Coverage: {coverage}%")
+
+    def calculate_overlap(self):
+        coveredArea = 0
+        overlapCount = 0
+
+        for (i, j), value in np.ndenumerate(self.coverage_map):
+            if value == 1:
+                coveredArea += 1
+            if value > 1:
+                overlapCount += 1
+                color = value_to_color(value)
+                pygame.draw.rect(self.screen, color, (i, j, 1, 1))
+
+        overlap = int((coveredArea / overlapCount) * 100)
+        print(f"Overlap: {overlap}%")
+        pygame.display.update()
+
     def run(self):
         clock = pygame.time.Clock()
 
@@ -153,5 +235,46 @@ class DroneSimulator:
             self.draw()
 
         self.print_info()
+        if self.boundry_shape == 'circle':
+            self.add_circle_boundry_to_coverage_map(self.boundry_params.get('x'), self.boundry_params.get('y'), self.boundry_params.get('radius'))
+        self.add_photos_to_coverage_map()
+
+        zero_positions = np.argwhere(self.coverage_map > 0)
+        for x, y in zero_positions:
+            pygame.draw.circle(self.screen, (123, 123, 123), (x, y), 1)
+        # pygame.display.update()
+
+        self.calculate_coverage()
+        self.calculate_overlap()
+        for point in self.path:
+            pygame.draw.circle(self.screen, PATH_COLOR, self.to_screen_coords(point), 1)
+        pygame.display.update()
         self.wait_for_exit()
         pygame.quit()
+
+
+# zero_positions = np.argwhere(self.coverage_map == 0)
+#         for x, y in zero_positions:
+#             pygame.draw.circle(self.screen, (123, 123, 123), (x, y), 1)
+#         pygame.display.update()
+
+def rotate_point(x, y, cx, cy, angle):
+    theta = np.radians(angle)
+    cos_t, sin_t = np.cos(theta), np.sin(theta)
+
+    # Translate point to origin (centerd at cx, cy)
+    x_shifted, y_shifted = x - cx, y - cy
+
+    # Apply rotation matrix
+    x_rot = cos_t * x_shifted - sin_t * y_shifted + cx
+    y_rot = sin_t * x_shifted + cos_t * y_shifted + cy
+
+    return int(round(x_rot)), int(round(y_rot))
+
+def value_to_color(value, vmin=2, vmax=10, cmap_name="plasma", alpha=128):
+    """ Maps a value to a color using a colormap (for values > 1) """
+    norm = (value - vmin) / (vmax - vmin)  # Normalize between 0 and 1
+    norm = np.clip(norm, 0, 1)  # Ensure values stay within 0-1 range
+    cmap = plt.get_cmap(cmap_name)  # Get colormap
+    r, g, b, _ = cmap(norm)  # Extract RGB (ignore alpha)
+    return (int(r * 255), int(g * 255), int(b * 255), alpha)  # Convert to 0-255 RGB
